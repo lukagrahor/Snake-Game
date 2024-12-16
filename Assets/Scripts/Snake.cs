84a759c2cfadcc2cf6aa7250a5c0fcc9bcc44a4e
Gaber <41000453+lukagrahor@users.noreply.github.com>
2024-12-16 18:51:39 +0100

    Prvi build na telefon

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,20 +1,42 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Snake : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] GameObject snakePart;
     SnakeMovement SnakeMovement;
-    void Start()
+
+    Vector3 moveDirection;
+    [SerializeField] float moveSpeed = 2f;
+    void Awake()
     {
+        moveDirection = new Vector3(1f, 0, 0);
         GameObject firstPart = Instantiate(snakePart);
         firstPart.transform.SetParent(transform);
-        firstPart.transform.localPosition = new Vector3(0, 1, 0);
+        // arena je na poziciji 0, kocka arene jevelika 1, kar pomeni da gre za 0.5 gor od 0, kocka od kaèe pa je velika 0.5 --> 0.25
+        firstPart.transform.localPosition = new Vector3(0f, 0.75f, 0f);
+        SnakeMovement = gameObject.AddComponent<SnakeMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(1 * Time.deltaTime, 0, 0);
+        Move(moveDirection);
+    }
+
+    public void Move(Vector3 direction)
+    {
+        transform.Translate(direction * Time.deltaTime);
+    }
+
+    public Vector3 GetDirection()
+    {
+        return moveDirection;
+    }
+
+    public void SetDirection(Vector3 direction)
+    {
+        moveDirection = direction * moveSpeed;
     }
 }
--- Assets/Scripts/SnakeMovement.cs
+++ Assets/Scripts/SnakeMovement.cs
@@ -1,16 +1,62 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SnakeMovement : MonoBehaviour
 {
+    InputSystem_Actions _controls;
+    [SerializeField] float minimumSwipeMagnitude = 10f;
+    Snake snake;
+    private Vector2 swipeDirection;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Barje");
+        snake = gameObject.GetComponent<Snake>();
+        _controls = new InputSystem_Actions();
+        _controls.Player.Enable();
+        _controls.Player.Touch.canceled += TouchCompleted;
+        _controls.Player.Swipe.performed += SwipePerformed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void TouchCompleted(InputAction.CallbackContext context)
+    {
+        if (swipeDirection.magnitude < minimumSwipeMagnitude) { return; }
+    }
+    private void SwipePerformed(InputAction.CallbackContext context)
+    {
+        Vector3 moveDirection = snake.GetDirection();
+        swipeDirection = context.ReadValue<Vector2>();
+        if (moveDirection.x != 0)
+        {
+            // Dol desno
+            if (swipeDirection.x > 0 && swipeDirection.y < 0)
+            {
+                snake.SetDirection(new Vector3(0, 0, -1f));
+            }
+            // Gor levo
+            else if (swipeDirection.x < 0 && swipeDirection.y > 0)
+            {
+                snake.SetDirection(new Vector3(0, 0, 1f));
+            }
+        }
+        else if (moveDirection.z != 0)
+        {
+            // Dol levo
+            if (swipeDirection.x > 0 && swipeDirection.y > 0)
+            {
+                snake.SetDirection(new Vector3(1f, 0, 0));
+            }
+            // Gor desno
+            else if (swipeDirection.x < 0 && swipeDirection.y < 0)
+            {
+                snake.SetDirection(new Vector3(-1f, 0, 0));
+            }
+        }
     }
 }
