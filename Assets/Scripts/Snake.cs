adea6dbcc7652da221fe96598eeb0dcff4daf2e6
Gaber <41000453+lukagrahor@users.noreply.github.com>
2025-02-26 21:48:00 +0100

    Nič

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -44,7 +44,7 @@ public class Snake : MonoBehaviour
 
     void Start()
     {
-        Application.targetFrameRate = 30;
+        //Application.targetFrameRate = 30;
     }
 
     // Update is called once per frame
