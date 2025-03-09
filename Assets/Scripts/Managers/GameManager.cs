c1a3f209ce5a8b88a93aeadacbda37c769a1f147
Gaber <41000453+lukagrahor@users.noreply.github.com>
2025-03-09 18:55:39 +0100

    Presortirana koda po mapah
    Pa dodan GameManager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void NewGame()
+    {
+
+    }
+
+    public void GameOver()
+    {
+
+    }
+}
