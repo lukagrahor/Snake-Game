491f85c40028489954e8eff7943698540e87698c
Gaber <41000453+lukagrahor@users.noreply.github.com>
2025-04-14 21:57:48 +0200

    AI nasprotnik preide v pursue state
    Dodan razred Countdown, ki je kot CountDownTimer, le da ni monobehaviour.

--- Assets/Scripts/Enemies/States/IdleState.cs
+++ Assets/Scripts/Enemies/States/IdleState.cs
@@ -6,19 +6,29 @@ public class IdleState : IState
     protected GameObject npc;
     protected Transform player;
     protected StateMachine stateMachine;
+    CountDown timer;
+    float waitTime = 3.0f;
     public IdleState(GameObject npc, Transform player, StateMachine stateMachine)
     {
         this.npc = npc;
         this.player = player;
         this.stateMachine = stateMachine;
+        timer = new CountDown(waitTime);
+        timer.TimeRanOut += StopWaiting;
     }
+    
+    void StopWaiting()
+    {
+        stateMachine.TransitionTo(stateMachine.pursueState);
+    }
+
     public void Enter()
     {
-        
+        timer.Start();
     }
     public void Update()
     {
-        
+        timer.Update();
     }
     public void Exit()
     {
---
+++ Assets/Scripts/Utility/Countdown.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+
+public class CountDown
+{
+    float timer = 0f;
+    bool isRunning = false;
+    public event Action TimeRanOut;
+    public CountDown (float timer)
+    {
+        this.timer = timer;
+    }
+
+    public void SetDuration(float duration)
+    {
+        timer = duration;
+    }
+
+    public void Start()
+    {
+        isRunning = true;
+    }
+
+    public void Update()
+    {
+        if (isRunning == true)
+        {
+            Use();
+        }
+    }
+
+    void Use()
+    {
+        if (timer > 0f)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            isRunning = false;
+            TimeRanOut?.Invoke();
+        }
+    }
+}
