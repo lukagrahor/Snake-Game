b19f07242372f5a5f55bbb4058c7d2b1241a4fd4
Gaber <41000453+lukagrahor@users.noreply.github.com>
2025-08-04 19:01:01 +0200

    Nastavitve grafike delujejo

--- Assets/Scripts/MainMenu/SettingsManager.cs
+++ Assets/Scripts/MainMenu/SettingsManager.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 public class SettingsManager : MonoBehaviour
 {
     public TMP_Dropdown graphicsDropdown;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     public void ChangeGraphicsQuality()
     {
+        /*if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+        }*/
         int graphicSetting = graphicsDropdown.value;
         Debug.Log("graphicSetting: " + graphicSetting);
         QualitySettings.SetQualityLevel(graphicSetting);
