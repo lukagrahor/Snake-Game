63f655d0f5575a62624ccdc532ddc4fe2ff2e001
Gaber <41000453+lukagrahor@users.noreply.github.com>
2025-08-05 23:06:32 +0200

    Dodana testna moč v trgovino

---
+++ Assets/Scripts/Game/Market/Powers/IPower.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface IPower
+{
+    public void Buy();
+}
---
+++ Assets/Scripts/Game/Market/Powers/TestPower.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class TestPower : MonoBehaviour, IPower
+{
+    public void Buy()
+    {
+        Debug.Log("Test item bought");
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
